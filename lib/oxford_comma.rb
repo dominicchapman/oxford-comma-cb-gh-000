ecb3a8a37b694a9e87519295ea93df78365b871c
Dominic Chapman <dominicchapman@me.com>
2017-10-03 22:16:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,19 @@
 def oxford_comma(array)
-  if (array.length == 1)
-    return array.join
-  elsif (array.length == 2)
-    return array.join(' and ')
-  elsif (array.length == 3)
-    return array[0] + ', ' + array[1] + ', and ' + array[2]
-  end
+  # if (array.length == 1)
+  #   return array.join
+  # elsif (array.length == 2)
+  #   return array.join(' and ')
+  # elsif (array.length == 3)
+  #   return array[0] + ', ' + array[1] + ', and ' + array[2]
+  # elsif (array.length > 3)
+  #   while array[i]
+  # end
 end
+
+
+# ['frog', 'spawn', 'marsh', 'mallow']
+# frog, spawn, marsh, and mallow
+
+# i.e. add comma to each item in array except last
+# plus, add ' and ' before last item in array
+
