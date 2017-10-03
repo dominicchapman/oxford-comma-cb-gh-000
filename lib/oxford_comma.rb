822e82f8cb6af5ffebc93d8edc796d573506bc16
Dominic Chapman <dominicchapman@me.com>
2017-10-03 22:07:31 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif (array.length == 2)
     return array.join(' and ')
   elsif (array.length == 3)
-    return array.join(', and ')
+    return array[0] + ', ' + array[1] + ', and ' array[2]
   end
 end
